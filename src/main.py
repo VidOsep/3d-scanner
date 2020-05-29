e0b841a34d85e487a3866d9cda98f0b05d91f178
VidOsep <vid.osep@gmail.com>
2020-05-29 14:42:58 +0200

    Added new file

--- src/main.py
+++ src/main.py
@@ -8,12 +8,13 @@ PORT = 42069
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connected = False
 
-def connect(HOST, PORT, s):
+def connect():
     """ Establish a connection between the raspberry pi and the pc """
+    global connected
     # Run the server side script on the raspberry pi
     os.system("ssh pi@%s nohup python server.py &"%HOST)  # Make sure your pi's ssh server is passwordless
     # Establish the connection
-    s.connect(HOST, PORT)
+    s.connect((HOST, PORT))
     reply = s.recv(1024)
     if reply == "Connected":
         print("Successfuly connected!")
@@ -25,5 +26,42 @@ def startNormalScan():
         print("Raspberry pi not connected!")
         return
     # Give signal to start scan
-    s.send(b"start-scan")
-    
+    s.send(b"start-scan-normal")
+    data = s.recv(1024)  # Wait for completion on server side
+    if data == "finished":
+        print("Successful scan")
+
+def testHardware():
+    # Check if device connected
+    if not connected:
+        print("Raspberry pi not connected!")
+        return
+    # Give signal to start test
+    s.send(b"test")
+    data = s.recv(1024) # Wait for completion on server side
+    if data == "finished":
+        print("Successful test")
+
+def quit():
+    # Check if device connected
+    if not connected:
+        print("Raspberry pi not connected!")
+        return
+    # Give signal to quit
+    s.send(b"quit")
+
+
+print("Connect - connect\nStart Normal Scan - ns\nTest Hardware - test\nQuit - quit")
+while True:
+    cmd = input("->")
+    if cmd=="connect":
+        connect()
+    elif cmd=="ns":
+        startNormalScan()
+    elif cmd=="test":
+        testHardware()
+    elif cmd=="lbls":
+        continue
+    elif cmd=="quit":
+        quit()
+        break
--- src/server-side/main.py
+++ src/server-side/main.py
@@ -10,12 +10,13 @@ import socket
 camera = picamera.PiCamera()
 
 # Laser control
+PIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(PIN,GPIO.OUT)
 
 # Stepper intialization
-mh = Adafruit_MotorHAT(addr = 0x60)
+mh = Adafruit_MotorHAT(addr=0x60)
 atexit.register(turnOffMotors)
 myStepper = mh.getStepper(200, 1)
 myStepper.setSpeed(30)
@@ -29,37 +30,49 @@ s.bind((HOST, PORT))
 s.listen(1)
 (conn, addr) = s.accept()
 
+
 def turnOffMotors():
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
- 
+
+def testHardware():
+    """ Tests the stepper motor and laser """
+    myStepper.step(100, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.STEP)
+    time.sleep(1)
+    myStepper.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.STEP)
+    time.sleep(1)
+    GPIO.output(PIN, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(PIN, GPIO.LOW)
+
 def normalScan(resolution=100):
     """ Scans whole object, announces completion to server """
-    for i in range(resolution)
+    for i in range(resolution):
         camera.capture('image' + str(i) + ".jpg")
         time.sleep(0.1)
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(PIN, GPIO.HIGH)
         camera.capture('subimage' + str(i) + ".jpg")
         time.sleep(0.1)
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(PIN, GPIO.LOW)
         myStepper.step(1, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
-
 
 def LBLScan():
     """ Scans object line by line, sends each pair of images indiviudally"""
     # TODO
     return
 
+while True:
+    data = conn.recv(1024) # Read incoming commands from pc
 
-
-for i in range(100):
-    myStepper.step(1, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
-    camera.capture('image' + str(i) + ".jpg")
-    print("1#taken")
-    time.sleep(0.2)	
-    GPIO.output(18,GPIO.HIGH)
-    camera.capture('subimage' + str(i) + ".jpg")
-    time.sleep(0.2)
-    GPIO.output(18,GPIO.LOW)
+    if data=="start-scan-normal":
+        normalScan()
+        s.send(b"finished")
+    elif data=="start-scan-lbl":
+        pass
+    elif data=="test":
+        testHardware()
+        s.send(b"finished")
+    elif data=="quit":
+        break
